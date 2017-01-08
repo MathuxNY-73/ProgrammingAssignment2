3f7434a925553e8f77bdefe5373339f142b57336
Antoine Pouillaude <pouillaude.a@gmail.com>
2017-01-08 22:05:32 +0100

    Fills the function definitions for matrix cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two following functions create a cache to compute and store the inverse  
+## of an invertible matrix. This optimize repetitive calls to matrix inversion operations. 
 
-## Write a short comment describing this function
+## Create a wrapper to cache the inverse of a given matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mInverse <- NULL;
+    set <- function(newMatrix) {
+        x <<- newMatrix;
+        mInverse <<- NULL;
+    };
+    get <- function() x
+    setInverse <- function(inverse) mInverse <<- inverse 
+    getInverse <- function() mInverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Retrieve the inverse of a wrapped matrix or compute it and store it in the cache wrapper. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse();
+
+    # If the inverse is NULL then we need to compute it.
+    if(!is.null(inverse)) {
+        message("Getting cached matrix inverse");
+        return(inverse);
+    }
+
+    # Retrieve the data of a wrapped matrix.
+    mat <- x$get();
+
+    # Compute the inverse of the matrix.
+    inverse <- solve(mat, ...);
+
+    # Store the computed inverse in the wrapper.
+    x$setInverse(inverse);
+    inverse
 }
